25f3353c8f37ff8805050521f02d2cd2c58d7b40
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-02-04 22:06:03 -0800

    Second graph comes out weird, third graph fails to render

--- public/index.js
+++ public/index.js
@@ -4,6 +4,132 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    //Fetch requests commented out because I was getting messages saying I had exceeded my API credits and I don't want to pay for more
+
+    /*const gme = await fetch('https://api.twelvedata.com/time_series?symbol=GME&interval=30min&apikey=4fc46edb6649483e97727c679337a986');
+    const gmeObj = await gme.json();
+    console.log(gmeObj);
+
+    const msft = await fetch('https://api.twelvedata.com/time_series?symbol=MSFT&interval=30min&apikey=4fc46edb6649483e97727c679337a986');
+    const msftObj = await msft.json();
+    console.log(msftObj);
+
+    const dis = await fetch('https://api.twelvedata.com/time_series?symbol=DIS&interval=30min&apikey=4fc46edb6649483e97727c679337a986');
+    const disObj = await dis.json();
+    console.log(disObj);
+
+    const bntx = await fetch('https://api.twelvedata.com/time_series?symbol=BNTX&interval=30min&apikey=4fc46edb6649483e97727c679337a986');
+    const bntxObj = await bntx.json();
+    console.log(bntxObj);*/
+
+    const { GME, MSFT, DIS, BNTX } = mockData;
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    stocks.forEach( stock => stock.values.reverse());
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map( value => value.datetime),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor:getColor(stock.meta.symbol)
+            }))
+        },
+    });
+    function getColor(stock){
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }
+    function getHighest(values){
+        let highest = values[0];
+        for(let i=1; i<values.length; i++){
+            if(values[i] > highest){
+                highest = values[i];
+            }
+        }
+        return highest;
+    }
+    
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: ['GME', 'MSFT', 'DIS', 'BTNX'],
+            datasets: [
+                {
+                    label: 'GME',
+                    data: getHighest(GME.values),
+                    backgroundColor: getColor('GME'),
+                    borderColor:getColor('GME')
+                },
+                {
+                    label: 'MSFT',
+                    data: getHighest(MSFT.values),
+                    backgroundColor: getColor('MSFT'),
+                    borderColor: getColor('MSFT')
+                },
+                {
+                    label: 'DIS',
+                    data: getHighest(DIS.values),
+                    backgroundColor: getColor('DIS'),
+                    borderColor: getColor('DIS')
+                },
+                {
+                    label: 'BNTX',
+                    data: getHighest(BNTX.values),
+                    backgroundColor: getColor('BNTX'),
+                    borderColor: getColor('BNTX')
+                }
+            ]
+        }
+    });
+    function getAverage(values){
+        let sum = 0;
+        for(let i=0; i<values.length; i++){
+            sum += values[i]
+        }
+        return sum/values.length
+    }
+    new Chart(averagePriceChartCanvas.getContext('2d'),{
+        type: 'pie',
+        data: {
+            labels: ['GME', 'MSFT', 'DIS', 'BTNX'],
+            datasets: [
+                {
+                    label: 'GME',
+                    data: getAverage(GME.values),
+                    backgroundColor: getColor('GME')
+                },
+                {
+                    label: 'MSFT',
+                    data: getAverage(MSFT.values),
+                    backgroundColor: getColor('MSFT')
+                },
+                {
+                    label: 'DIS',
+                    data: getAverage(DIS.values),
+                    backgroundColor: getColor('DIS')
+                },
+                {
+                    label: 'BNTX',
+                    data: getAverage(BNTX.values),
+                    backgroundColor: getColor('BNTX')
+                }
+            ]
+        }
+    })
 }
 
 main()
